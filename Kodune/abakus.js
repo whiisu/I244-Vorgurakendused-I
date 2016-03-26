b79952ad162d1d5bd4f3df39bd7c4d36d1a85809
kaernits <whiisu@msn.com>
2016-03-26 14:03:12 +0200

    jQ praktikum

--- Kodune/abakus.js
+++ Kodune/abakus.js
@@ -3,12 +3,12 @@ window.addEventListener ('load', function();{
 	var i;
 	for (i = 0; i < parlid.length; i++){
 		var cssFloat = parlid[i];
-		cssFloat.onclick = function(){
+		cssFloat.onclick = function();{
 		if (this.style.sccFloat == "right"){
 			this.style.cssFloat = "left";
 		} else {
 			this.style.sccFloat = "right";
 		}
-	}
+		}
 	}
 });
