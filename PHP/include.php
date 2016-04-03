b5b622296d384e9000903465fb94c2268736e524
kaernits <whiisu@msn.com>
2016-04-03 20:36:33 +0300

    7. kodune massiiv kristallidega

---
+++ PHP/include.php
@@ -0,0 +1,15 @@
+
+<?php
+$kristallid= array( 
+		array('nimi'=>'Delfiin', 'kaal'=>250, 'hind'=>150, 'pilt'=>'../img/delfiin.jpeg', 'autor'=>'madaqascarminerals'), 
+		array('nimi'=>'Elevant', 'kaal'=>750, 'hind'=>500, 'pilt'=>'../img/elevant.jpeg', 'autor'=>'wpclipart'),
+		array('nimi'=>'Kolju', 'kaal'=>150, 'hind'=>200, 'pilt'=>'../img/kolju.jpeg', 'autor'=>'scullis'),
+		array('nimi'=>'Kuju', 'kaal'=>100, 'hind'=>175, 'pilt'=>'../img/kuju.jpeg', 'autor'=>'ebth'),
+		array('nimi'=>'Kuul', 'kaal'=>450, 'hind'=>350, 'pilt'=>'../img/kuul.jpeg', 'autor'=>'madaqascarminerals'),
+		array('nimi'=>'Pea', 'kaal'=>350, 'hind'=>200, 'pilt'=>'../img/pea.jpeg', 'autor'=>'healing-crystals'),
+		array('nimi'=>'Siga', 'kaal'=>100, 'hind'=>150, 'pilt'=>'../img/siga.jpeg', 'autor'=>'qia'),
+		array('nimi'=>'Süda', 'kaal'=>75, 'hind'=>75, 'pilt'=>'../img/süda.jpeg', 'autor'=>'bestcrystals'),
+		array('nimi'=>'Täht', 'kaal'=>300, 'hind'=>500, 'pilt'=>'../img/täht.jpeg', 'autor'=>'wamduskasapa'),
+	);
+include('kristall.html');
+?>
