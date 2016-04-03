e45abad86609db81566d6bf526c9e93bc0b34636
kaernits <whiisu@msn.com>
2016-04-03 18:46:51 +0300

    praktikum 7

--- kontroller.php
+++ kontroller.php
@@ -2,15 +2,15 @@ ini_set("display_errors", 1);
 
 <?php 
 $pildid=array(
-  array("big"=>"img/delfiin.jpeg", "small"=>"thumb/delfiin_thumb.jpeg", "alt"=>"delfiin"),
-  array("big"=>"img/elevant.jpeg", "small"=>"thumb/elevant_thumb.jpeg", "alt"=>"elevant"),
-  array("big"=>"img/kolju.jpeg", "small"=>"thumb/kolju_thumb.jpeg", "alt"=>"kolju"),
-  array("big"=>"img/kuju.jpeg", "small"=>"thumb/kuju_thumb.jpeg", "alt"=>"kuju"),
-  array("big"=>"img/kuul.jpeg", "small"=>"thumb/kuul_thumb.jpeg", "alt"=>"kuul"),
-  array("big"=>"img/pea.jpeg", "small"=>"thumb/pea_thumb.jpeg", "alt"=>"pea"),
-  array("big"=>"img/siga.jpeg", "small"=>"thumb/siga_thumb.jpeg", "alt"=>"siga"),
-  array("big"=>"img/süda.jpeg", "small"=>"thumb/süda_thumb.jpeg", "alt"=>"süda"),
-  array("big"=>"img/täht.jpeg", "small"=>"thumb/täht_thumb.jpeg", "alt"=>"täht"),	
+  array("big"=>"../img/delfiin.jpeg", "small"=>"../thumb/delfiin_thumb.jpeg", "alt"=>"delfiin"),
+  array("big"=>"../img/elevant.jpeg", "small"=>"../thumb/elevant_thumb.jpeg", "alt"=>"elevant"),
+  array("big"=>"../img/kolju.jpeg", "small"=>"../thumb/kolju_thumb.jpeg", "alt"=>"kolju"),
+  array("big"=>"../img/kuju.jpeg", "small"=>"../thumb/kuju_thumb.jpeg", "alt"=>"kuju"),
+  array("big"=>"../img/kuul.jpeg", "small"=>"../thumb/kuul_thumb.jpeg", "alt"=>"kuul"),
+  array("big"=>"../img/pea.jpeg", "small"=>"../thumb/pea_thumb.jpeg", "alt"=>"pea"),
+  array("big"=>"../img/siga.jpeg", "small"=>"../thumb/siga_thumb.jpeg", "alt"=>"siga"),
+  array("big"=>"../img/süda.jpeg", "small"=>"../thumb/süda_thumb.jpeg", "alt"=>"süda"),
+  array("big"=>"../img/täht.jpeg", "small"=>"../thumb/täht_thumb.jpeg", "alt"=>"täht"),	
  );
  include_once("view/head.html");
  include("view/Praktikum2_galerii.html");
