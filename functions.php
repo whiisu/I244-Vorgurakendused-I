c41150e65216847879d6b47c2952875430b68c60
kaernits <whiisu@msn.com>
2016-04-17 20:55:51 +0300

    function.php

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php 
 
-
-
+$mode="esileht"; 		
+	if (isset($_GET["mode"]) && $_GET["mode"]!="") {
+   	 $mode=htmlspecialchars($_GET["mode"]); 
+	}
+include_once "head.html";
+	
+switch ($mode) {
+	case "esileht": include "Praktikum1_esileht.html";
+	break;
+	case "galerii": include "Praktikum2_galerii.html"
+	break;
+	case "login": include "Praktikum2_login.html";
+	break;
+	case "register": include "Praktikum2_registreerimine.html"
+	break;
+	case "pildilaadimine": include "Praktikum2_pildi_laadimine.html"
+	break;
+	default: include "Praktikum1_esileht.html";
+	break;
+}
+include_once "foot.html";
 ?>
